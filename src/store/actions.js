21610fac37e679f812b35191bf8eb794d67cea7c
Kento75 <kento2github@gmail.com>
2018-10-26 06:29:41 +0900

    Add apis

---
+++ src/store/actions.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-unused-vars */
+import * as types from './mutation-types'
+import { Auth, List, Task } from '../api'
+/* eslint-enable no-unused-vars */
+
+export default {
+  // ログインアクション
+  login: ({ commit }) => {
+    // TODO:
+    throw new Error('login action should be implemented')
+  },
+
+  // 全件取得アクション
+  fetchLists: ({ commit }) => {
+    // TODO:
+    throw new Error('fetchLists action should be implemented')
+  },
+
+  // タスク追加アクション
+  addTask: ({ commit }) => {
+    // TODO:
+    throw new Error('addTask action should be implemented')
+  },
+
+  // タスク更新アクション
+  updateTask: ({ commit }) => {
+    // TODO:
+    throw new Error('updateTask action should be implemented')
+  },
+
+  // タスク削除アクション
+  removeTask: ({ commit }) => {
+    // TODO:
+    throw new Error('removeTask action should be implemented')
+  },
+
+  // ログアウトアクション
+  logout: ({ commit }) => {
+    // TODO:
+    throw new Error('logout action should be implemented')
+  },
+}
--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,19 @@ import mutations from './mutations'
 
 Vue.use(Vuex)
 
+// 状態 Auth と状態 Board を Vuex の state で一言管理できるように定義
+const state = {
+  auth: {
+    token: null,
+    userId: null,
+  },
+  board: {
+    lists: [],
+  },
+}
+
 export default new Vuex.Store({
+  state,
   getters,
   actions,
   mutations,
